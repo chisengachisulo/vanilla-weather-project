52245f2ecff3f1eb4384a642a5949eb3b0260b5a
chisengachisulo <128135390+chisengachisulo@users.noreply.github.com>
2023-09-12 13:21:09 +0100

    Added weather forecast to js

--- src/app.js
+++ src/app.js
@@ -9,16 +9,38 @@ function formatDate(timestamp) {
     minutes = `0${minutes}`;
   }
   let days = [
+    "Sunday",
     "Monday",
     "Tuesday",
     "Wednesday",
     "Thursday",
     "Friday",
     "Saturday",
-    "Sunday",
   ];
   let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`;
+}
+
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Wed", "Thu", "Fri", "Sat", "Sun", "Mon"];
+  let forecastHTML = `<div class ="row">`;
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `<div class="col-2"> 
+        <div class="weather-forecast-date">${day}</div>
+          <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/few-clouds-day.png"    alt="" width="42"> 
+          <div class="weather-forecast-temperature"> 
+            <span class="weather-forecast-temperature-max">18°</span> 
+            <span class="weather-forecast-temperature-min">12°</span>
+        </div>
+      </div>`;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
 }
 
 function displayTemperature(response) {
@@ -83,3 +105,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("london");
+displayForecast();
